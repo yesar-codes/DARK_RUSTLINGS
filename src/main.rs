4bccc0f16ba4ae94030c9f5ccd1fa8bd6f716fd7
usyzk <usyzk@student.kit.edu>
2026-04-25 10:37:50 +0200

    Open Game by default in Fullscreen

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,24 @@
 use bevy::asset::AssetPlugin;
 use bevy::prelude::*;
+use bevy::window::{MonitorSelection, WindowMode, WindowPlugin};
 mod game;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins.set(AssetPlugin {
-            file_path: "resources".into(),
-            ..default()
-        }))
+        .add_plugins(
+            DefaultPlugins
+                .set(AssetPlugin {
+                    file_path: "resources".into(),
+                    ..default()
+                })
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        mode: WindowMode::BorderlessFullscreen(MonitorSelection::Primary),
+                        ..default()
+                    }),
+                    ..default()
+                }),
+        )
         .add_plugins(game::GamePlugin)
         .run();
 }
